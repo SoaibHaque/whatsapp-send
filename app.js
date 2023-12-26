e5d2aaa8fec2a5c0469026c02462a887f7e6b208
Soaib Haque <soaibhaque3@gmail.com>
2023-12-26 22:17:31 +0530

    Added both web and api link

--- app.js
+++ app.js
@@ -1,32 +1,47 @@
 (function () {
+  function formSubmit(e) {
+    e.preventDefault(); // prevent from send the request by own
 
-    function formSubmit(e) {
-        e.preventDefault(); // prevent from send the request by own
-
-        // opening whatsapp api link
-        let url = this.action;
-        url += this.countryCode.value + this.phoneNumber.value;
-        window.open(url, '_blank');
+    if (
+      localStorage.openInApplication &&
+      JSON.parse(localStorage.openInApplication)
+    ) {
+      this.action = "https://api.whatsapp.com/send?phone=";
+    } else {
+      this.action = "https://web.whatsapp.com/send?phone=";
     }
 
-    function validNumber() {
-        if (this.value.length == 10) {
-            this.parentElement.classList.remove('invalid');
-            this.parentElement.classList.add('valid');
-        } else {
-            this.parentElement.classList.remove('valid');
-            this.parentElement.classList.add('invalid');
-        }
+    // opening whatsapp api link
+    let url = this.action;
+    url += this.countryCode.value + this.phoneNumber.value;
+    window.open(url, "_blank");
+  }
+
+  function validNumber() {
+    if (this.value.length == 10) {
+      this.parentElement.classList.remove("invalid");
+      this.parentElement.classList.add("valid");
+    } else {
+      this.parentElement.classList.remove("valid");
+      this.parentElement.classList.add("invalid");
     }
+  }
 
-    function stopKeypress(e) {
-        if (this.value.length >= 10 && e.which != 13) e.preventDefault(); // stop from typing number more than 10
-    }
+  function stopKeypress(e) {
+    if (this.value.length >= 10 && e.which != 13) e.preventDefault(); // stop from typing number more than 10
+  }
 
-    const [form] = document.forms; // get the zeroth index form
-    form.phoneNumber.focus(); // take the focus to the phone number ihput
-    form.addEventListener('submit', formSubmit); // run when form submit
-    form.phoneNumber.addEventListener('input', validNumber); // check the number is value or not
-    form.phoneNumber.addEventListener('keypress', stopKeypress); // check the number is value or not
+  function changeOption() {
+    localStorage.openInApplication = this.checked;
+  }
 
-})(); // immediately invok function
+  const [form] = document.forms; // get the zeroth index form
+  form.phoneNumber.focus(); // take the focus to the phone number input
+  form.addEventListener("submit", formSubmit); // run when form submit
+  form.phoneNumber.addEventListener("input", validNumber); // check the number is value or not
+  form.phoneNumber.addEventListener("keypress", stopKeypress); // check the number is value or not
+  form.open_application.addEventListener("change", changeOption);
+  form.open_application.checked =
+    localStorage.openInApplication &&
+    JSON.parse(localStorage.openInApplication);
+})(); // immediately invoke function
